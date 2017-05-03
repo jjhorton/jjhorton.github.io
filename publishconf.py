6d37cf83b7ff3ef8a55b9f6d3744e1f1d6f51cec
James Horton <jjhorton@gmail.com>
2017-05-03 20:31:42 +0100

    added theme the config file

--- publishconf.py
+++ publishconf.py
@@ -17,6 +17,7 @@ FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
+THEME = "/home/jjh/Documents/website/pelican-theme"
 
 # Following items are often useful when publishing
 
